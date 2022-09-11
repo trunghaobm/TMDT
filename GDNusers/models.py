1e2226310586db46466b7a33e30090f94abc0306
Gideon <haoden2@gmail.com>
2022-09-12 02:13:27 +0700

    Xong layout

--- GDNusers/models.py
+++ GDNusers/models.py
@@ -1,3 +1,10 @@
+from itertools import chain
+from operator import mod, truediv
 from django.db import models
 
 # Create your models here.
+class category(models.Model):
+    id = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=100)
+    count = models.CharField(max_length=100)
+        
--- GDNwebsite/settings.py
+++ GDNwebsite/settings.py
@@ -82,7 +82,11 @@ WSGI_APPLICATION = 'GDNwebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',               
+        'USER': 'admin',                      
+        'PASSWORD': 'admin',                 
+        'HOST': 'localhost',                    
+        'PORT': '9000'
     }
 }
 
@@ -121,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
